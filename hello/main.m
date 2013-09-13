a0a3ac2b9167761961be3da1760fd34f43dd4519
Hammad Ahmed <ahmed.hammad10@gmail.com>
2013-09-12 20:01:54 -0500

    Initial Commit

---
+++ hello/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hello
+//
+//  Created by Hammad  on 9/12/13.
+//  Copyright (c) 2013 Hammad Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
